f00a5c227aaee33dbb7aee20f22ab9654c187aed
Gabriel <gabrielpereiradev@outlook.com>
2022-03-29 19:12:57 -0300

    create Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Text} from 'react-native';
-import { Container, Title } from './styles';
 
-export function Dashboard(){
-    return(
+
+import {
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserName,
+    UserGreeting,
+    UserWrapper,
+    Icon
+
+} from './styles';
+
+export function Dashboard() {
+    return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: "https://avatars.githubusercontent.com/u/86992261?v=4" }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>
+                                Gabriel
+                            </UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
